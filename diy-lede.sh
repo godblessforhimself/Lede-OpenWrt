1932ec759919966e1b29f5e1dd2e781fc86c5498
godblessforhimself <godblessforhimself@users.noreply.github.com>
2020-12-25 08:57:38 +0800

    Update diy-lede.sh

--- diy-lede.sh
+++ diy-lede.sh
@@ -8,7 +8,7 @@ rm -rf package/lean/luci-theme-argon
 #
 
 # 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了，其他的不要动
-sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
 
 
 #修改内核版本（下面两行代码前面有#为源码默认最新5.4内核,没#为4.19内核,默认修改X86的，其他机型L大那里target/linux查看，对应修改下面的路径就好）
@@ -25,24 +25,24 @@ sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz
 
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge package/luci-theme-edge  #主题-edge-动态登陆界面
-git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash  #openclash出国软件
-git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash  #clash出国软件
-git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan  #微信推送
+#git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash  #openclash出国软件
+#git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash  #clash出国软件
+#git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan  #微信推送
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns  #smartdns DNS加速
 
 
 #passwall出国软件
-svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-app-passwall package/luci-app-passwall
-svn co https://github.com/Lienol/openwrt-package/trunk/package/brook package/brook
-svn co https://github.com/Lienol/openwrt-package/trunk/package/chinadns-ng package/chinadns-ng
-svn co https://github.com/Lienol/openwrt-package/trunk/package/tcping package/tcping
-svn co https://github.com/Lienol/openwrt-package/trunk/package/trojan-go package/trojan-go
-svn co https://github.com/Lienol/openwrt-package/trunk/package/trojan-plus package/trojan-plus
-svn co https://github.com/Lienol/openwrt-package/trunk/package/syncthing package/syncthing
+#svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-app-passwall package/luci-app-passwall
+#svn co https://github.com/Lienol/openwrt-package/trunk/package/brook package/brook
+#svn co https://github.com/Lienol/openwrt-package/trunk/package/chinadns-ng package/chinadns-ng
+#svn co https://github.com/Lienol/openwrt-package/trunk/package/tcping package/tcping
+#svn co https://github.com/Lienol/openwrt-package/trunk/package/trojan-go package/trojan-go
+#svn co https://github.com/Lienol/openwrt-package/trunk/package/trojan-plus package/trojan-plus
+#svn co https://github.com/Lienol/openwrt-package/trunk/package/syncthing package/syncthing
 
 
 #luci-app-jd-dailybonus[京东签到]，luci-app-vssr [Hello World-出国软件-经常订阅不了节点，不知何解]，luci-theme-argon-18.06[argon-主题]
-sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
+#sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 
 #全新的[argon-主题]登录界面,图片背景跟随Bing.com，每天自动切换
 #增加可自定义登录背景功能，请自行将文件上传到/www/luci-static/argon/background 目录下，支持jpg png gif格式图片，主题将会优先显示自定义背景，多个背景为随机显示，系统默认依然为从bing获取
